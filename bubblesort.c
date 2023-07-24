09a7148b09aa5487dd1ff3f188410224687ab229
Deepadd04 <140432023+Deepadd04@users.noreply.github.com>
2023-07-24 22:31:58 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i,j,temp;
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	{
+		for(i=0;i<n;i++)
+		{
+			if(a[i]<a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+}
